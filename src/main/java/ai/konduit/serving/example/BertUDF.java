ecdfe6816428801413473b6a98f5590ec0ebfaea
Eduardo Gonzalez <me@wm-eddie.com>
2020-01-24 12:47:06 +0900

    First shot at custom UDF.

---
+++ src/main/java/ai/konduit/serving/example/BertUDF.java
@@ -0,0 +1,93 @@
+package ai.konduit.serving.example;
+
+import ai.konduit.serving.pipeline.CustomPipelineStepUDF;
+import org.datavec.api.records.Record;
+import org.datavec.api.writable.NDArrayWritable;
+import org.datavec.api.writable.Writable;
+import org.deeplearning4j.iterator.BertIterator;
+import org.deeplearning4j.iterator.LabeledSentenceProvider;
+import org.deeplearning4j.text.tokenization.tokenizerfactory.BertWordPieceTokenizerFactory;
+import org.nd4j.linalg.dataset.api.MultiDataSet;
+import org.nd4j.linalg.primitives.Pair;
+
+import java.io.File;
+import java.io.IOException;
+import java.nio.charset.StandardCharsets;
+import java.util.Collections;
+import java.util.List;
+
+public class BertUDF implements CustomPipelineStepUDF, LabeledSentenceProvider{
+    private final BertIterator bertIterator;
+    private Record[] currentRecords;
+
+
+    public BertUDF() {
+        super();
+        String path = System.getProperty("bertudf.vocab");
+        File vocabFile = new File(path);
+
+        try {
+
+            BertWordPieceTokenizerFactory tokenizer = new BertWordPieceTokenizerFactory(vocabFile, true, true, StandardCharsets.UTF_8);
+            bertIterator = BertIterator.builder()
+                    .tokenizer(tokenizer)
+                    .lengthHandling(BertIterator.LengthHandling.FIXED_LENGTH, 100)
+                    .minibatchSize(1)
+                    .sentenceProvider(this)
+                    .featureArrays(BertIterator.FeatureArrays.INDICES_MASK)
+                    .vocabMap(tokenizer.getVocab())
+                    .task(BertIterator.Task.SEQ_CLASSIFICATION)
+                    .build();
+        } catch (IOException e) {
+            throw new IllegalStateException("Vocabulary file missing.  Check that JVM property -Dbert.vocab is set porperly.", e);
+        }
+    }
+
+
+    @Override
+    public Record[] udf(Record[] records) {
+        if (records == null) {
+            throw new NullPointerException("records can not be null.");
+        }
+
+        currentRecords = records;
+        MultiDataSet dataSet = bertIterator.next();
+        NDArrayWritable writable = new NDArrayWritable(dataSet.getFeatures()[0]);
+        org.datavec.api.records.impl.Record ret = new org.datavec.api.records.impl.Record(Collections.singletonList(writable), null);
+        return new Record[] { ret };
+    }
+
+    //region LabeledSentenceProvider Methods
+
+    @Override
+    public boolean hasNext() {
+        return currentRecords != null;
+    }
+
+    @Override
+    public Pair<String, String> nextSentence() {
+        List<Writable> writables = currentRecords[0].getRecord();
+        return Pair.of(writables.get(0).toString(), "");
+    }
+
+    @Override
+    public void reset() {
+
+    }
+
+    @Override
+    public int totalNumSentences() {
+        return 0;
+    }
+
+    @Override
+    public List<String> allLabels() {
+        return Collections.singletonList("");
+    }
+
+    @Override
+    public int numLabelClasses() {
+        return 1;
+    }
+    //endregion
+}
---
+++ src/test/java/ai/konduit/serving/example/AppTest.java
@@ -0,0 +1,65 @@
+package ai.konduit.serving.example;
+
+import org.apache.commons.io.FileUtils;
+import org.apache.commons.lang3.StringUtils;
+import org.datavec.api.records.impl.Record;
+import org.datavec.api.writable.NDArrayWritable;
+import org.datavec.api.writable.Text;
+import org.junit.BeforeClass;
+import org.junit.Test;
+
+import java.io.File;
+import java.io.IOException;
+import java.io.InputStream;
+import java.net.URL;
+import java.nio.charset.StandardCharsets;
+import java.util.Collections;
+
+import static org.junit.Assert.*;
+
+/**
+ * Unit test for simple App.
+ */
+public class AppTest {
+
+    @BeforeClass
+    public static void beforeTest() throws IOException {
+        InputStream resource = AppTest.class.getResourceAsStream("vocab.txt");
+        File tempFile = File.createTempFile(String.valueOf(resource.hashCode()), ".tmp");
+        tempFile.deleteOnExit();
+        FileUtils.copyInputStreamToFile(resource, tempFile);
+        System.setProperty("bertudf.vocab", tempFile.getAbsolutePath());
+    }
+
+    @Test
+    public void shouldAnswerWithTrue() {
+        assertTrue(true);
+    }
+
+    @Test
+    public void testUdfWithNullRecords() {
+        try {
+
+            new BertUDF().udf(null);
+            fail("Should thow NPE.");
+        } catch (NullPointerException e){
+            assertTrue(true);
+        }
+    }
+
+    @Test
+    public void testUdfWithSentence() {
+        String input = "i have a pen i have a pineapple";
+        Text inputText = new Text(input);
+        Record record = new Record(Collections.singletonList(inputText), null);
+        org.datavec.api.records.Record[] outputs = new BertUDF().udf(new org.datavec.api.records.Record[]{record});
+        assertNotNull(outputs);
+        assertTrue(outputs.length > 0);
+        assertTrue(outputs[0].getRecord().get(0) instanceof NDArrayWritable);
+
+        NDArrayWritable writable = (NDArrayWritable)outputs[0].getRecord().get(0);
+        assertEquals(2, writable.get().rank());
+        assertEquals(1, writable.get().shape()[0]);
+        assertEquals(100, writable.get().shape()[1]);
+    }
+}
